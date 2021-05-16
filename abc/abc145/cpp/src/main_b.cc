f906ccdfaca55c61ec3c13989c3f31b41c2a789a
pollenjp <polleninjp@gmail.com>
2021-05-16 04:05:16 +0000

    Add abc145 main_b.cc

---
+++ abc/abc145/cpp/src/main_b.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+bool solve(int n, std::string s) {
+  if (n % 2 != 0) {
+    return false;
+  };
+  std::string sub_str = s.substr(0, s.length() / 2);
+  return sub_str == s.substr(s.length() / 2, s.length() / 2);
+}
+
+int main() {
+  int N;
+  std::string S;
+  std::cin >> N >> S;
+
+  std::string answer;
+  if (solve(N, S)) {
+    answer = "Yes";
+
+  } else {
+    answer = "No";
+  }
+  std::cout << answer << std::endl;
+  return EXIT_SUCCESS;
+}
