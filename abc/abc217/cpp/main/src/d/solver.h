42e126150738fda27fb0d65841b015172c415d26
pollenjp <polleninjp@gmail.com>
2021-09-04 14:53:04 +0000

    Update abc217d

--- abc/abc217/cpp/main/src/d/solver.h
+++ abc/abc217/cpp/main/src/d/solver.h
@@ -1,23 +1,24 @@
 #include <algorithm>
 #include <exception>
 #include <iostream>
+#include <set>
 #include <utility>
 #include <vector>
 
 namespace abc217 {
 void solve(const int l, const std::vector<std::pair<int, int>> &q_vec) {
-  std::vector<int> cut_x_idx_vec;
-  cut_x_idx_vec.push_back(0);
-  cut_x_idx_vec.push_back(l);
+  std::set<int> cut_x_idx_set;
+  cut_x_idx_set.insert(0);
+  cut_x_idx_set.insert(l);
 
-  for (auto &q_vec_i : q_vec) {
+  for (const auto &q_vec_i : q_vec) {
     auto c_i = q_vec_i.first;
     auto x_i = q_vec_i.second;
     if (c_i == 1) {
-      cut_x_idx_vec.insert(std::upper_bound(cut_x_idx_vec.begin(), cut_x_idx_vec.end(), x_i), x_i);
+      cut_x_idx_set.insert(x_i);
     } else if (c_i == 2) {
-      auto itr = std::upper_bound(cut_x_idx_vec.begin(), cut_x_idx_vec.end(), x_i);
-      auto val = (*itr) - *(itr - 1);
+      auto itr = cut_x_idx_set.lower_bound(x_i);
+      auto val = (*itr) - *(std::prev(itr, 1));
       std::cout << val << std::endl;
     } else {
       throw std::runtime_error("unexpected value: c_i");
