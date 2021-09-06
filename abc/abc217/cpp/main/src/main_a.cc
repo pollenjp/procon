72dfebb39b2f7d5d31af90af44236ac5c3c0434b
pollenjp <polleninjp@gmail.com>
2021-09-06 17:10:46 +0000

    Update abc217 main_a.cc - add try catch

--- abc/abc217/cpp/main/src/main_a.cc
+++ abc/abc217/cpp/main/src/main_a.cc
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 #include "a/solver.h"
 
 int main() {
-  std::string s, t;
-  std::cin >> s >> t;
+  try {
+    std::string s;
+    std::string t;
+    std::cin >> s >> t;
 
-  std::cout << abc217::solve(s, t) << std::endl;
-  return EXIT_SUCCESS;
+    std::cout << abc217::solve(s, t) << std::endl;
+    return EXIT_SUCCESS;
+  } catch (std::exception& e) {
+    std::cerr << "error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  } catch (...) {
+    return EXIT_FAILURE;
+  }
 }
