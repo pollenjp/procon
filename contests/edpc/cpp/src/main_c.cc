d74436f0b91da82b09b2412478c5f6ee2e6aa270
pollenjp <polleninjp@gmail.com>
2021-10-21 12:56:16 +0000

    Add contests/edpc/cpp/src/c/

---
+++ contests/edpc/cpp/src/main_c.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "c/solver.h"
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  int32_t num_n(0);  // 1 <= num_n <= 10^5
+
+  std::cin >> num_n;
+
+  LOG(INFO) << "Input num_n = " << num_n;
+
+  std::vector<int32_t> a_vec(num_n, 0), b_vec(num_n, 0), c_vec(num_n, 0);  // 1 <= a_i, b_i, c_i <= 10^4
+
+  for (int32_t i = 0; i < num_n; i++) {
+    std::cin >> a_vec[i] >> b_vec[i] >> c_vec[i];
+  }
+
+  std::cout << atcoder::edpc::c::solve(num_n, a_vec, b_vec, c_vec) << std::endl;
+
+  return EXIT_SUCCESS;
+}
---
+++ contests/edpc/cpp/test/c/solver_test.cc
@@ -0,0 +1,26 @@
+#include "c/solver.h"
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace atcoder {
+
+namespace edpc {
+
+namespace c {
+
+TEST(SolverCTest, Sample1) {
+  int32_t num_n(3);
+  std::vector<int32_t> a_vec({10, 20, 30});
+  std::vector<int32_t> b_vec({40, 50, 60});
+  std::vector<int32_t> c_vec({70, 80, 90});
+
+  EXPECT_EQ(solve(num_n, a_vec, b_vec, c_vec), 210);
+}
+
+}  // namespace c
+
+}  // namespace edpc
+
+}  // namespace atcoder
