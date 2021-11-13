50d724ef56f78ddbf8a5332a0c8f05472ef2e422
pollenjp <polleninjp@gmail.com>
2021-11-13 11:21:38 +0000

    Refactoring abc/abc226/b/cpp

--- abc/abc226/b/cpp/src/main.cc
+++ abc/abc226/b/cpp/src/main.cc
@@ -1,13 +1,17 @@
+#include <pollenlib/solver.h>
+
 #include <iostream>
-#include <set>
 #include <vector>
 
 int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   int32_t num_n(0);  // 1 <= num_n <= 10^5
 
   std::cin >> num_n;
 
-  std::set<std::vector<int32_t>> l_list_set;
+  std::vector<std::vector<int32_t>> l_list_list(num_n);
 
   for (int32_t i = 0; i < num_n; ++i) {
     int32_t num_l(0);
@@ -16,11 +20,10 @@ int main(int argc, char* argv[]) {
     for (int32_t j = 0; j < num_l; ++j) {
       std::cin >> l_list[j];
     }
-
-    l_list_set.insert(l_list);
+    l_list_list[i] = std::move(l_list);
   }
 
-  std::cout << l_list_set.size() << std::endl;
+  std::cout << atcoder::abc226::b::solve(l_list_list) << std::endl;
 
   return EXIT_SUCCESS;
 }
--- abc/abc226/b/cpp/src/pollenlib/solver.h
+++ abc/abc226/b/cpp/src/pollenlib/solver.h
@@ -1,7 +1,7 @@
 #include <glog/logging.h>
 
-#include <algorithm>
 #include <numeric>
+#include <set>
 #include <string>
 #include <utility>
 #include <vector>
@@ -12,32 +12,12 @@ namespace abc226 {
 
 namespace b {
 
-int32_t solve(std::vector<std::vector<int32_t>> const &l_list_list) {
-  std::vector<int32_t> sum_list(l_list_list.size());
-
-  for (std::size_t i = 0; i < l_list_list.size(); ++i) {
-    sum_list[i] = std::accumulate(l_list_list[i].begin(), l_list_list[i].end(), 0);
+std::size_t solve(const std::vector<std::vector<int32_t>> &l_list_list) {
+  std::set<std::vector<int32_t>> bin_tree;
+  for (auto &l_list : l_list_list) {
+    bin_tree.insert(l_list);
   }
-
-  int32_t cnt_duplicate(0);
-  for (std::size_t i = 0; i < l_list_list.size() - 1; ++i) {
-    for (std::size_t j = i + 1; j < l_list_list[i].size(); ++j) {
-      if (sum_list[i] == sum_list[j] && l_list_list[i].size() == l_list_list[j].size()) {
-        // element check
-        bool is_same(true);
-        for (std::size_t k = 0; k < l_list_list[i].size(); ++k) {
-          if (l_list_list[i][k] != l_list_list[j][k]) {
-            is_same = false;
-            break;
-          }
-        }
-        if (is_same) {
-          cnt_duplicate++;
-        }
-      }
-    }
-  }
-  return static_cast<int32_t>(l_list_list.size()) - cnt_duplicate;
+  return bin_tree.size();
 }
 
 }  // namespace b
