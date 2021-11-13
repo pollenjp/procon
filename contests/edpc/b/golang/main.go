8806b438c30de78483336d789aa3857a26071a7c
pollenjp <polleninjp@gmail.com>
2021-11-13 07:08:56 +0000

    fix lint

--- contests/edpc/b/golang/main.go
+++ contests/edpc/b/golang/main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	var (
-		n int;
-		numK int;
+		n    int
+		numK int
 	)
 	fmt.Scanf("%d %d", &n, &numK)
 	h_slice := make([]int, n)
@@ -23,9 +23,9 @@ func main() {
 	for i := int(1); i < n; i++ {
 		for k := int(1); k <= numK; k++ {
 			if k == 1 {
-				cost_dp[i] = cost_dp[i-k] + int(math.Abs(float64(h_slice[i] - h_slice[i-k])))
-			} else if i - k >= 0{
-				cost_dp[i] = min(cost_dp[i], cost_dp[i-k] + int(math.Abs(float64(h_slice[i] - h_slice[i-k]))))
+				cost_dp[i] = cost_dp[i-k] + int(math.Abs(float64(h_slice[i]-h_slice[i-k])))
+			} else if i-k >= 0 {
+				cost_dp[i] = min(cost_dp[i], cost_dp[i-k]+int(math.Abs(float64(h_slice[i]-h_slice[i-k]))))
 			} else {
 				break
 			}
@@ -41,23 +41,12 @@ func main() {
 ///////////
 
 func min(nums ...int) int {
-    if len(nums) == 0 {
-        panic("funciton min() requires at least one argument.")
-    }
-    res := nums[0]
-    for i := 0; i < len(nums); i++ {
-        res = int(math.Min(float64(res), float64(nums[i])))
-    }
-    return res
-}
-
-func max(nums ...int) int {
-    if len(nums) == 0 {
-        panic("funciton max() requires at least one argument.")
-    }
-    res := nums[0]
-    for i := 0; i < len(nums); i++ {
-        res = int(math.Max(float64(res), float64(nums[i])))
-    }
-    return res
+	if len(nums) == 0 {
+		panic("funciton min() requires at least one argument.")
+	}
+	res := nums[0]
+	for i := 0; i < len(nums); i++ {
+		res = int(math.Min(float64(res), float64(nums[i])))
+	}
+	return res
 }
