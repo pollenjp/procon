66a5b2b2491c4fe16d4bf22c0da946bcbd0f293c
pollenjp <polleninjp@gmail.com>
2022-02-20 02:05:12 +0900

    Add abc239 e (RE)

--- abc/abc239/main_e.py
+++ abc/abc239/main_e.py
@@ -10,18 +10,22 @@ logging.basicConfig(
 logger = getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+CONST_MAX_K: int = 20
+
 
 @dataclass
 class Node:
+    value: int
     parent: Optional[int] = None
     children: Set[int] = field(default_factory=set)
+    inv_sorted_list: Optional[List[int]] = None
 
 
 def main():
 
     input_n, input_q = map(int, input().rstrip().split())
     x_list = list(map(int, input().rstrip().split()))
-    logger.info(f"{x_list=}")
+    # logger.info(f"{x_list=}")
 
     connect_set_list = [set() for _ in range(input_n)]  # 0 origin
     for _ in range(input_n - 1):
@@ -29,25 +33,45 @@ def main():
         connect_set_list[x - 1].add(y - 1)
         connect_set_list[y - 1].add(x - 1)
 
-    node_list: List[Node] = [Node() for i in range(input_n)]
+    node_list: List[Node] = [Node(value=x_val) for x_val in x_list]
 
-    def create_node(current_node: int):
-        if len(connect_set_list[current_node]) == 0:  # leaf
+    def create_node(current_node_idx: int):
+        if len(connect_set_list[current_node_idx]) == 0:  # leaf
             return
-        for child in list(connect_set_list[current_node]):
-            connect_set_list[current_node].remove(child)
-            connect_set_list[child].remove(current_node)
+        current_node: Node = node_list[current_node_idx]
+        for child_idx in list(connect_set_list[current_node_idx]):
+            connect_set_list[current_node_idx].remove(child_idx)
+            connect_set_list[child_idx].remove(current_node_idx)
 
-            node_list[current_node].children.add(child)
-            node_list[child].parent = current_node
+            current_node.children.add(child_idx)
+            node_list[child_idx].parent = current_node_idx
 
-            create_node(child)
+            create_node(child_idx)
 
-    create_node(current_node=0)
-    logger.info(f"{node_list}")
+    create_node(0)
+    # logger.info(f"{node_list=}")
+
+    # recursive sort
+    def set_sort_list(current_node_idx: int):
+        current_node: Node = node_list[current_node_idx]
+        if len(current_node.children) == 0:
+            current_node.inv_sorted_list = [current_node.value]
+            return
+
+        values: List[int] = [current_node.value]
+        for child_idx in current_node.children:
+            set_sort_list(child_idx)
+            values += node_list[child_idx].inv_sorted_list
+
+        current_node.inv_sorted_list = sorted(values, reverse=True)[:CONST_MAX_K]
+
+    set_sort_list(0)
+    # logger.info(f"{node_list=}")
 
     for _ in range(input_q):
-        x, y = map(int, input().rstrip().split())
+        k, v = map(int, input().rstrip().split())
+        node_k: Node = node_list[k - 1]
+        print(f"{node_k.inv_sorted_list[v-1]}")
 
 
 if __name__ == "__main__":
