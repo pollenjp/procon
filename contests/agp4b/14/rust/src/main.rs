8ee9407fe387eaa4c48f5719cd5c955a692abaf6
pollenjp <polleninjp@gmail.com>
2022-04-23 19:38:33 +0900

    Update

--- contests/agp4b/14/rust/src/main.rs
+++ contests/agp4b/14/rust/src/main.rs
@@ -1,8 +1,10 @@
+#![warn(clippy::pedantic, clippy::nursery)]
+
 fn main() {
     proconio::input! {
         abc: [i32; 3],
     }
 
     dbg!(abc.iter().min().unwrap(), abc.iter().max().unwrap());
-    println!("{}", abc.iter().max().unwrap() - abc.iter().min().unwrap())
+    println!("{}", abc.iter().max().unwrap() - abc.iter().min().unwrap());
 }
