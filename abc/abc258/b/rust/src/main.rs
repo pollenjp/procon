db1ecdd6f8bdfc121715893116c49a5e5c9c7a08
pollenjp <polleninjp@gmail.com>
2022-07-17 20:26:33 +0900

    update

--- abc/abc258/b/rust/src/main.rs
+++ abc/abc258/b/rust/src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
+use std::time::Instant;
 
 // modulo i32
 // return one of the 0..=m-1
@@ -38,16 +40,18 @@ fn main() {
         table_a: &Vec<Vec<i32>>,
         max_visit: i32,
         coord: (i32, i32),
-        visited: &HashSet<(i32, i32)>,
+        visited: &mut HashSet<(i32, i32)>,
         current_num: i32,
     ) -> i32 {
         if visited.len() == max_visit as usize {
             return current_num;
         }
 
+        let time = Instant::now();
+
         let current_num = current_num * 10 + table_a[coord.0 as usize][coord.1 as usize];
         let (row_num, col_num) = (table_a.len() as i32, table_a[0].len() as i32);
-        let mut visited = visited.clone();
+        // let mut visited = visited.clone();
         visited.insert(coord);
 
         let coord_offset = vec![
@@ -61,41 +65,39 @@ fn main() {
             (-1, -1), // up-left
         ];
 
-        let mut coords = HashSet::new();
+        type CoordMap = HashMap<(i32, i32), i32>;
+        let mut coords: CoordMap = HashMap::new();
         for (r, c) in coord_offset {
             let next_coord = (modi(coord.0 + r, row_num), modi(coord.1 + c, col_num));
             if visited.contains(&next_coord) {
                 continue;
             }
-            coords.insert(next_coord);
+            let (r, c) = next_coord;
+            coords.insert(next_coord, table_a[r as usize][c as usize]);
         }
 
-        let val_vec = coords
-            .iter()
-            .map(|&(x, y)| table_a[x as usize][y as usize])
-            .collect::<Vec<i32>>();
-        if val_vec.len() == 0 {
+        if coords.len() == 0 {
             return -1; // no more path
         }
 
-        let coord_vec = coords.iter().collect::<Vec<_>>();
-        let max_val = val_vec.iter().max().unwrap();
-
-        let val_vec_with_idx = val_vec
-            .clone()
-            .into_iter()
-            .enumerate()
-            .filter(|(_, v)| v == max_val)
-            .collect::<Vec<(usize, i32)>>();
+        let max_val = coords.iter().map(|(_, v)| *v).max().unwrap();
 
         let mut max_ret = 0;
-        for (i, _) in val_vec_with_idx {
-            let &(r, c) = coord_vec[i];
-            let ret_s = dfs(table_a, max_visit, (r, c), &visited, current_num);
+        for (&(r, c), &v) in coords.iter() {
+            if v < max_val {
+                continue;
+            }
+            let ret_s = dfs(table_a, max_visit, (r, c), visited, current_num);
             if ret_s > max_ret {
                 max_ret = ret_s;
             }
         }
+
+        if visited.len() == 4 {
+            dbg!(time.elapsed(), current_num);
+        }
+
+        visited.remove(&coord);
 
         return max_ret;
     }
@@ -106,7 +108,7 @@ fn main() {
             &table_a,
             in_n as i32,
             (i as i32, j as i32),
-            &HashSet::new(),
+            &mut HashSet::from([(i as i32, j as i32)]),
             0,
         );
         if current_num > max_num {
