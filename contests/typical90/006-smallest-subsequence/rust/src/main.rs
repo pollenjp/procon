0f4c990a6db04ca020dd0eb25e3f0562cdad80a4
pollenjp <polleninjp@gmail.com>
2022-07-20 17:47:47 +0900

    fix format

--- contests/typical90/006-smallest-subsequence/rust/src/main.rs
+++ contests/typical90/006-smallest-subsequence/rust/src/main.rs
@@ -13,29 +13,29 @@ fn main() {
 
     let mut mp_start_idx: usize = 0;
     let mut mp_next_start_idx: usize = 0;
-    let mp_end_idx: usize = in_s.len()-in_k+1;
+    let mp_end_idx: usize = in_s.len() - in_k + 1;
     let mut tgt_chars: Vec<char> = Vec::new();
 
     for k in 0..in_k {
         // k番目のcharを選択
 
-        for i in mp_next_start_idx..mp_end_idx+k {
+        for i in mp_next_start_idx..mp_end_idx + k {
             let c = in_s.chars().nth(i).unwrap();
             if mp.contains_key(&c) {
-                mp.insert(c, mp[&c]+1);
+                mp.insert(c, mp[&c] + 1);
             } else {
                 mp.insert(c, 1);
             }
         }
-        mp_next_start_idx = mp_end_idx+k;
+        mp_next_start_idx = mp_end_idx + k;
 
         let (&tgt_c, _) = mp.first_key_value().unwrap();
         tgt_chars.push(tgt_c);
 
-        for i in mp_start_idx..mp_end_idx+k {
+        for i in mp_start_idx..mp_end_idx + k {
             let c = in_s.chars().nth(i).unwrap();
             if mp.contains_key(&c) {
-                mp.insert(c, mp[&c]-1);
+                mp.insert(c, mp[&c] - 1);
                 if mp[&c] == 0 {
                     mp.remove(&c);
                 }
@@ -44,7 +44,7 @@ fn main() {
             }
 
             if c == tgt_c {
-                mp_start_idx = i+1;
+                mp_start_idx = i + 1;
                 break;
             }
         }
