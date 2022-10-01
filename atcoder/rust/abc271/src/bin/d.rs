674dc05f165fc97d56e6ab79a7495c134c78f025
pollenjp <polleninjp@gmail.com>
2022-10-01 22:56:46 +0900

    Refactoring abc271/d

--- atcoder/rust/abc271/src/bin/d.rs
+++ atcoder/rust/abc271/src/bin/d.rs
@@ -4,7 +4,7 @@ fn main() {
     input! {
         in_n: usize,
         in_s: usize,
-        in_ab: [(usize, usize); in_n],
+        in_ab: [[usize; 2]; in_n],
     }
 
     // s[i][j] := i番目まで選択した時にの和がj
@@ -15,25 +15,16 @@ fn main() {
     dp[0][0] = 0;
 
     for i in 0..in_n {
-        let x = in_ab[i].0;
-        for j in 0..=in_s {
-            if j < x {
-                continue;
+        for (label, &x) in in_ab[i].iter().enumerate() {
+            for j in 0..=in_s {
+                if j < x {
+                    continue;
+                }
+                if dp[i][j - x] == -1 {
+                    continue;
+                }
+                dp[i + 1][j] = label as i32;
             }
-            if dp[i][j - x] == -1 {
-                continue;
-            }
-            dp[i + 1][j] = 0;
-        }
-        let x = in_ab[i].1;
-        for j in 0..=in_s {
-            if j < x {
-                continue;
-            }
-            if dp[i][j - x] == -1 {
-                continue;
-            }
-            dp[i + 1][j] = 1;
         }
     }
 
@@ -47,12 +38,13 @@ fn main() {
     let mut ans_rev = vec![];
     let mut k = in_s as i64;
     for i in (0..in_n).rev() {
-        if dp[i + 1][k as usize] == 0 {
+        let j = dp[i + 1][k as usize] as usize;
+        if j == 0 {
             ans_rev.push('H');
-            k -= in_ab[i].0 as i64;
+            k -= in_ab[i][j] as i64;
         } else {
             ans_rev.push('T');
-            k -= in_ab[i].1 as i64;
+            k -= in_ab[i][j] as i64;
         }
     }
 
